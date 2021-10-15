71fdbd7ae9b0ec1dcb831593c639222b92cbf521
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-10-15 20:05:26 +0300

    исправлено предупреждение консоли о неверной ссылке

--- project/src/components/main-navigation/main-navigation.jsx
+++ project/src/components/main-navigation/main-navigation.jsx
@@ -4,19 +4,19 @@ function MainNavigation() {
   return (
     <ul className="main-nav">
       <li className="main-nav__item">
-        <a className="main-nav__link" href="#">Услуги</a>
+        <a className="main-nav__link" href="/">Услуги</a>
       </li>
       <li className="main-nav__item">
-        <a className="main-nav__link" href="#">Рассчитать кредит</a>
+        <a className="main-nav__link" href="/">Рассчитать кредит</a>
       </li>
       <li className="main-nav__item">
-        <a className="main-nav__link main-nav__link--current" href="#">Конвертер валют</a>
+        <a className="main-nav__link main-nav__link--current" href="/">Конвертер валют</a>
       </li>
       <li className="main-nav__item">
-        <a className="main-nav__link" href="#">Контакты</a>
+        <a className="main-nav__link" href="/">Контакты</a>
       </li>
       <li className="main-nav__item">
-        <a className="main-nav__link" href="#">Задать вопрос</a>
+        <a className="main-nav__link" href="/">Задать вопрос</a>
       </li>
     </ul>
   );
