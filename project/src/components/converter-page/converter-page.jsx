0fb720054a723b95b362be5b680df123d816ada4
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-10-16 00:16:05 +0300

    начало раметки и стилизации блока с конвертером

--- project/src/components/converter-page/converter-page.jsx
+++ project/src/components/converter-page/converter-page.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Logo from '../logo/logo';
 import UserBlock from '../user-block/user-block';
-import Social from '../social/social';
 import Converter from '../converter/converter';
 import ConverterHistory from '../converter-history/converter-history';
 import MainNavigation from '../main-navigation/main-navigation';
+import Promo from '../promo/promo';
+import Footer from '../footer/footer';
 
 function ConverterPage() {
   return (
@@ -15,47 +16,13 @@ function ConverterPage() {
         <UserBlock/>
       </header>
       <main>
-        <div className="promo">
-          <h1 className="promo__header">Лига Банк</h1>
-          <h2 className="promo__text">Кредиты на любой случай</h2>
-          <a className="promo__link" href="#">Рассчитать кредит</a>
-        </div>
+        <Promo/>
         <div className="content">
           <Converter/>
           <ConverterHistory/>
         </div>
       </main>
-      <footer>
-        <div className="contacts">
-          <Logo/>
-          <p className="address">150015, г. Москва, ул. Московская, д. 32 Генеральная лицензия Банка России №1050 Ⓒ Лига Банк, 2019</p>
-        </div>
-        <ul className="nav">
-          <li className="nav__item">
-            <a className="nav__link" href="#">Услуги</a>
-          </li>
-          <li className="nav__item">
-            <a className="nav__link" href="#">Рассчитать кредит</a>
-          </li>
-          <li className="nav__item">
-            <a className="nav__link" href="#">Контакты</a>
-          </li>
-          <li className="nav__item">
-            <a className="nav__link" href="#">Задать вопрос</a>
-          </li>
-        </ul>
-        <ul className="phone">
-          <li className="phone__item">
-            <a className="phone__link" href="tel:+0904">*0904</a>
-            <p>Бесплатно для абонентов МТС, Билайн, Мегафон, Теле2</p>
-          </li>
-          <li className="phone__item">
-            <a className="phone__link" href="tel:+88001112223">8 800 111 22 33</a>
-            <p>Бесплатный для всех городов России</p>
-          </li>
-        </ul>
-        <Social/>
-      </footer>
+      <Footer/>
     </>
   );
 }
