a4e1399641a2248b05277c96f6bdd81beff23b8c
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-10-15 22:14:35 +0300

    разметка блока promo

---
+++ project/src/components/promo/promo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Promo() {
+  return (
+    <div className="promo">
+      <h1 className="promo__header">Лига Банк</h1>
+      <h2 className="promo__text">Кредиты на любой случай</h2>
+      <a className="promo__link" href="/">Рассчитать кредит</a>
+    </div>
+  );
+}
+
+export default Promo;
