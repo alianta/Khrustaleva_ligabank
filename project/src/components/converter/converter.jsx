2868d12c6e82998cef2d0f21ca6c3efcd9a2baa9
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-10-18 00:28:58 +0300

    пересчет значений при смене валюты
    тестовый значения курсов валют (не из стораджа)

--- project/src/components/converter/converter.jsx
+++ project/src/components/converter/converter.jsx
@@ -15,6 +15,24 @@ function Converter() {
     purchasedCurrencyRate: 71.2371,
   });
 
+  const handleCurrencyChange = ({target}) => {
+    if(target.id === 'purchased-currency') {
+      setMoney({
+        ...money,
+        purchasedCurrency: target.value,
+        purchasedCurrencyRate: 50,//подставить значение из справочника
+        purchasedMoney: currencyConversation(money.avaliableMoney,money.avaliableCurrencyRate,50),//подставить значение из справочника вместо 50
+      });
+    } else {
+      setMoney({
+        ...money,
+        avaliableCurrency: target.value,
+        avaliableCurrencyRate: 30,//подставить значение из справочника
+        avaliableMoney: currencyConversation(money.purchasedMoney, money.purchasedCurrencyRate, 30),//подставить значение из справочника вместо 30
+      });
+    }
+  };
+
   const handleMoneyChange = ({target}) => {
     if(target.id === AVALIABLE_MONEY_INPUT_NAME) {
       setMoney({
@@ -37,7 +55,7 @@ function Converter() {
       <fieldset className="converter-money converter-money--avaliable">
         <label className="converter-money__label" htmlFor="available-money">У меня есть</label>
         <input id="available-money" className="converter-money__input" name="available-money" type="number" placeholder="1000" min="1" max="1000000" value={money.avaliableMoney} onChange={handleMoneyChange}/>
-        <select name="available-currency" id="available-currency" className="currency currency--avaliable">
+        <select name="available-currency" id="available-currency" className="currency currency--avaliable" onChange={handleCurrencyChange}>
           <option value="rub" selected>RUB</option>
           <option value="usd">USD</option>
           <option value="eur">EUR</option>
@@ -48,12 +66,12 @@ function Converter() {
       <fieldset className="converter-money converter-money--purchased">
         <label className="converter-money__label" htmlFor="purchased-money">Хочу приобрести</label>
         <input id="purchased-money" className="converter-money__input" name="purchased-money" type="number" placeholder="1000" min="1" max="1000000" step="0.0001" value={money.purchasedMoney} onChange={handleMoneyChange}/>
-        <select name="purchased-currency" id="purchased-currency" className="currency currency--purchased">
-          <option value="rub" >RUB</option>
-          <option value="usd" selected>USD</option>
-          <option value="eur">EUR</option>
-          <option value="gbr">GBR</option>
-          <option value="cny">CNY</option>
+        <select name="purchased-currency" id="purchased-currency" className="currency currency--purchased" onChange={handleCurrencyChange}>
+          <option value="RUB" >RUB</option>
+          <option value="USD" selected>USD</option>
+          <option value="EUR">EUR</option>
+          <option value="GBR">GBR</option>
+          <option value="CNY">CNY</option>
         </select>
       </fieldset>
 
